292ce3ffba9430d4609b03911dde39988a0400b4
Tom <thomas.spencer@codurance.com>
2020-04-30 19:29:14 +0100

    Initialize repository

---
+++ src/components/Column.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class Column extends React.Component {
+  render(){
+    return(
+        <div>
+          "Hello World"
+        </div>
+      )
+  }
+}
+
+export default Column;
