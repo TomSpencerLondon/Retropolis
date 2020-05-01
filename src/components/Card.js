0dea0d44fc20f1be94e1880e6e72bb9667aba4b6
Tom <thomas.spencer@codurance.com>
2020-05-01 21:06:17 +0100

    Refactor Card to include Display and Edit components

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,31 +3,50 @@ import './Card.css';
 import {useState} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
+import * as PropTypes from "prop-types";
+
+
+
 export default function Card() {
   const [editing, setEditing] = useState(true)
 
-   return(
+  return(
     editing ?
       <div className="new-back">
         <div className="message-body">
-          <textarea>
-          </textarea>
-          <button className="add-message" onClick={() => setEditing(false)}>
-            Add
-          </button>
+          <Edit onClick={() => setEditing(false)}/>
         </div>
       </div>
-:
+      :
       <div className="message-main">
         <div className="message-body">
-          <div className="text">
-            This card is working
-            <div className="pencil message-bodylink">
-              <FontAwesomeIcon icon={ faPencilAlt } onClick={() => setEditing(true)} />
-            </div>
-          </div>
-
+          <Display onClick={() => setEditing(true)}/>
         </div>
       </div>
-     )
+  )
 }
+
+function Display(props) {
+  return <div className="text">
+    This card is working
+    <div className="pencil message-bodylink">
+      <FontAwesomeIcon icon={faPencilAlt} onClick={props.onClick}/>
+    </div>
+  </div>;
+}
+
+Display.propTypes = {onClick: PropTypes.func};
+
+
+function Edit(props) {
+  return <>
+<textarea>
+          </textarea>
+    <button className="add-message" onClick={props.onClick}>
+      Add
+    </button>
+  </>;
+}
+
+Edit.propTypes = {onClick: PropTypes.func};
+
