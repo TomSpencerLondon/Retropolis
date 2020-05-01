954e851ede13df4518e1ad7a226dc7f25f4b1935
Tom <thomas.spencer@codurance.com>
2020-05-01 20:34:47 +0100

    Add edit icon

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,33 @@
 import * as React from "react";
 import './Card.css';
+import {useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
+export default function Card() {
+  const [editing, setEditing] = useState(true)
 
-class Card extends React.Component {
-  render() {
    return(
-     <div className="message-main">
-       <div className="message-body">
-         <div className="text">
-           This card is working
-         </div>
-       </div>
-     </div>
-     )
-  }
-}
+    editing ?
+      <div className="new-back">
+        <div className="message-body">
+          <textarea>
+          </textarea>
+          <button className="add-message" onClick={() => setEditing(false)}>
+            Add
+          </button>
+        </div>
+      </div>
+:
+      <div className="message-main">
+        <div className="message-body">
+          <div className="text">
+            This card is working
+            <div className="pencil message-bodylink">
+              <FontAwesomeIcon icon={ faPencilAlt } onClick={() => setEditing(true)} />
+            </div>
+          </div>
 
-export default Card;
+        </div>
+      </div>
+     )
+}
